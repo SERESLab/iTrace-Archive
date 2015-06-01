f309b389282d71872ee61f4becf3ac79700c930b
Jenna Wise <jennawise@hotmail.com>
2015-06-01 01:26:32 -0400

    Changed one more int to long.

--- src/edu/ysu/itrace/filters/Fixation.java
+++ src/edu/ysu/itrace/filters/Fixation.java
@@ -4,7 +4,7 @@ public class Fixation {
 	private RawGaze rawGaze;
 	private long duration;
 	
-	public Fixation(RawGaze rawGaze, int duration) {
+	public Fixation(RawGaze rawGaze, long duration) {
 		this.rawGaze = rawGaze;
 		this.duration = duration;
 	}
