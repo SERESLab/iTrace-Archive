3f3f0288cab28c53607bf78e4a665ed4013bdfb8
Ben Clark <onebenclark@gmail.com>
2016-06-10 18:42:32 -0400

    Sets export solvers to match master

--- src/edu/ysu/itrace/solvers/JSONGazeExportSolver.java
+++ src/edu/ysu/itrace/solvers/JSONGazeExportSolver.java
@@ -5,7 +5,6 @@ import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
@@ -18,9 +17,12 @@ import org.eclipse.core.resources.ResourcesPlugin;
 
 import com.google.gson.stream.JsonWriter;
 
-import edu.ysu.itrace.Activator;
 import edu.ysu.itrace.AstManager.SourceCodeEntity;
+import edu.ysu.itrace.SOManager.StackOverflowEntity;
+import edu.ysu.itrace.BRManager.BugReportEntity;
 import edu.ysu.itrace.gaze.IGazeResponse;
+import edu.ysu.itrace.gaze.IStackOverflowGazeResponse;
+import edu.ysu.itrace.gaze.IBugReportGazeResponse;
 import edu.ysu.itrace.gaze.IStyledTextGazeResponse;
 
 /**
@@ -92,6 +94,7 @@ public class JSONGazeExportSolver implements IFileExportSolver {
                         (int) (screenRect.width * response.getGaze().getX());
                 int screenY =
                         (int) (screenRect.height * response.getGaze().getY());
+
                 responseWriter.beginObject()
                               .name("name")
                               .value(response.getName())
@@ -109,12 +112,13 @@ public class JSONGazeExportSolver implements IFileExportSolver {
                               .value(response.getGaze().getLeftPupilDiameter())
                               .name("right_pupil_diameter")
                               .value(response.getGaze().getRightPupilDiameter())
-                              .name("timestamp")
-                              .value(response.getGaze().getTimestamp())
-                              .name("session_time")
-                              .value(response.getGaze().getSystemTime()-Activator.getDefault().sessionStartTime);
-                              
-                try {
+                              .name("tracker_time")
+                              .value(response.getGaze().getTrackerTime().getTime())
+                              .name("system_time")
+                              .value(response.getGaze().getSystemTime())
+                              .name("nano_time")
+                              .value(response.getGaze().getNanoTime());
+                if (response instanceof IStyledTextGazeResponse) {
                     IStyledTextGazeResponse styledResponse =
                             (IStyledTextGazeResponse) response;
                     responseWriter.name("path")
@@ -155,8 +159,52 @@ public class JSONGazeExportSolver implements IFileExportSolver {
                     }
                     responseWriter.endArray();
 
-                } catch (ClassCastException e) {
-                    // not styled text, oh well
+                } else if (response instanceof IStackOverflowGazeResponse) {
+                	IStackOverflowGazeResponse stackOverflowResponse =
+                            (IStackOverflowGazeResponse) response;
+                	StackOverflowEntity soe = stackOverflowResponse.getSOE();
+                    responseWriter.name("url")
+                                  .value(stackOverflowResponse.getURL())
+                                  .name("Id")
+                                  .value(stackOverflowResponse.getID())
+                                  .name("soe")
+                                  .beginObject()
+                                  	.name("part")
+                                  	.value(soe.part.toString())
+                                  	.name("part_number")
+                                  	.value(soe.partNum)
+                                  	.name("type")
+                                  	.value(soe.type.toString())
+                                  	.name("type_number")
+                                  	.value(soe.typeNum)
+                                  .endObject();
+                                  
+                }
+                
+                else if (response instanceof IBugReportGazeResponse) {
+                	IBugReportGazeResponse bugReportResponse =
+                            (IBugReportGazeResponse) response;
+                	BugReportEntity bre = bugReportResponse.getBRE();
+                    responseWriter.name("url")
+                                  .value(bugReportResponse.getURL())
+                                  .name("Id")
+                                  .value(bugReportResponse.getID())
+                                  .name("bre")
+                                  .beginObject()
+                                  	.name("part")
+                                  	.value(bre.part.toString())
+                                  	.name("part_number")
+                                  	.value(bre.partNum)
+                                  	.name("type")
+                                  	.value(bre.type.toString())
+                                  	.name("type_number")
+                                  	.value(bre.typeNum)
+                                  .endObject();
+                                  
+                }
+                
+                else {
+                	//ignore anything else
                 }
                 responseWriter.endObject();
         } catch (IOException e) {
--- src/edu/ysu/itrace/solvers/XMLGazeExportSolver.java
+++ src/edu/ysu/itrace/solvers/XMLGazeExportSolver.java
@@ -5,7 +5,6 @@ import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
@@ -19,9 +18,12 @@ import javax.xml.stream.XMLStreamWriter;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 
-import edu.ysu.itrace.Activator;
 import edu.ysu.itrace.AstManager.SourceCodeEntity;
+import edu.ysu.itrace.SOManager.StackOverflowEntity;
+import edu.ysu.itrace.BRManager.BugReportEntity;
 import edu.ysu.itrace.gaze.IGazeResponse;
+import edu.ysu.itrace.gaze.IStackOverflowGazeResponse;
+import edu.ysu.itrace.gaze.IBugReportGazeResponse;
 import edu.ysu.itrace.gaze.IStyledTextGazeResponse;
 
 /**
@@ -94,17 +96,13 @@ public class XMLGazeExportSolver implements IFileExportSolver {
     @Override
     public void process(IGazeResponse response) {
         try {
-        	try {
-                IStyledTextGazeResponse styledResponse =
-                        (IStyledTextGazeResponse) response;
-        		
                 int screenX =
                         (int) (screenRect.width * response.getGaze().getX());
                 int screenY =
                         (int) (screenRect.height * response.getGaze().getY());
-                
+
                 responseWriter.writeStartElement("response");
-                responseWriter.writeAttribute("name", styledResponse.getName());
+                responseWriter.writeAttribute("name", response.getName());
                 responseWriter.writeAttribute("type", response.getGazeType());
                 responseWriter.writeAttribute("x", String.valueOf(screenX));
                 responseWriter.writeAttribute("y", String.valueOf(screenY));
@@ -119,24 +117,19 @@ public class XMLGazeExportSolver implements IFileExportSolver {
                         String.valueOf(response.getGaze()
                                        .getRightPupilDiameter()));
                 responseWriter.writeAttribute(
-                        "timestamp",
-                        response.getGaze().getTimestamp());
-                
-                       	
+                        "tracker_time",
+                        String.valueOf(response.getGaze().getTrackerTime()
+                                .getTime()));
                 responseWriter.writeAttribute(
-                        "session_time",
-                        String.valueOf(response.getGaze().getSessionTime()));
+                        "system_time",
+                        String.valueOf(response.getGaze().getSystemTime()));
                 responseWriter.writeAttribute(
-                        "tracker/system/nano",
-                        String.valueOf(response.getGaze().getTrackerTime())+"/"+
-                        String.valueOf(response.getGaze().getSystemTime())+"/"+
+                        "nano_time",
                         String.valueOf(response.getGaze().getNanoTime()));
-                
-                /*
-                try {
+
+                if (response instanceof IStyledTextGazeResponse) {
                     IStyledTextGazeResponse styledResponse =
-                            (IStyledTextGazeResponse) response;*/
-                
+                            (IStyledTextGazeResponse) response;
                     responseWriter.writeAttribute("path", styledResponse.getPath());
                     responseWriter.writeAttribute("line_height",
                             String.valueOf(styledResponse.getLineHeight()));
@@ -170,8 +163,35 @@ public class XMLGazeExportSolver implements IFileExportSolver {
                     }
                     responseWriter.writeEndElement();
 
-                } catch (ClassCastException e) {
-                    // not styled text, oh well
+                } else if (response instanceof IStackOverflowGazeResponse) {
+                	IStackOverflowGazeResponse stackOverflowResponse =
+                            (IStackOverflowGazeResponse) response;
+                	StackOverflowEntity soe = stackOverflowResponse.getSOE();
+                    responseWriter.writeAttribute("url", stackOverflowResponse.getURL());
+                    responseWriter.writeAttribute("Id", stackOverflowResponse.getID());
+                    responseWriter.writeStartElement("soe");
+                    responseWriter.writeAttribute("part", soe.part.toString());
+                    responseWriter.writeAttribute("part_number", String.valueOf(soe.partNum));
+                    responseWriter.writeAttribute("type", soe.type.toString());
+                    responseWriter.writeAttribute("type_number", String.valueOf(soe.typeNum));
+                    responseWriter.writeEndElement();
+                } 
+                else if (response instanceof IBugReportGazeResponse) {
+                	IBugReportGazeResponse bugReportResponse =
+                            (IBugReportGazeResponse) response;
+                	BugReportEntity bre = bugReportResponse.getBRE();
+                    responseWriter.writeAttribute("url", bugReportResponse.getURL());
+                    responseWriter.writeAttribute("Id", bugReportResponse.getID());
+                    responseWriter.writeStartElement("bre");
+                    responseWriter.writeAttribute("part", bre.part.toString());
+                    responseWriter.writeAttribute("part_number", String.valueOf(bre.partNum));
+                    responseWriter.writeAttribute("type", bre.type.toString());
+                    responseWriter.writeAttribute("type_number", String.valueOf(bre.typeNum));
+                    responseWriter.writeEndElement();
+                }
+                
+                else {
+                	//ignore anything else
                 }
                 responseWriter.writeEndElement();
                 responseWriter.writeCharacters(EOL);
