7c4831a0e65f9b84a3a1e98882b34708395d3822
Ben Clark <onebenclark@gmail.com>
2016-10-18 00:06:29 -0400

    Draws vectors based on distance from points.

--- src/edu/ysu/itrace/CalibrationStatusDisplay.java
+++ src/edu/ysu/itrace/CalibrationStatusDisplay.java
@@ -32,7 +32,7 @@ public class CalibrationStatusDisplay extends JPanel {
 			int height = dims.height-(insets.top+insets.bottom);
 			
 			windowDimension = new Dimension(width,height);
-			
+			if(point == null) continue;
 			int x = (int)(point.x*windowDimension.getWidth());
 			int y = (int)(point.y*windowDimension.getHeight());
 			grphx.drawOval(x-40,y-40,80,80);
@@ -40,28 +40,43 @@ public class CalibrationStatusDisplay extends JPanel {
 		}
 		int i = 0;
 		for(Point2D.Double point: calibrationData){
+			if(point == null) continue;
+			if( ( point.x <= 0 || point.x >= 1 ) && ( point.y <= 0 || point.y >= 1 ) ){
+				i++;
+				continue;
+			}
 			int x = (int)(point.x*windowDimension.getWidth());
 			int y = (int)(point.y*windowDimension.getHeight());
 			if(i%2==0) grphx.setColor(Color.green);
 			else grphx.setColor(Color.red);
 			grphx.fillOval(x-2, y-2, 4, 4);
-			grphx.drawString(""+i,x-4,y-4);
-			for(int index = 0; index < 9; index++){
-				if(i < Math.ceil(((index+1)*(double)calibrationData.length/9))){
-					grphx.drawLine(x, y, (int)(calibrationPoints[index].x*windowDimension.getWidth()),(int)(calibrationPoints[index].y*windowDimension.getHeight()));
-					break;
+			//grphx.drawString(""+i,x-4,y-4);
+			int calibx = (int)(calibrationPoints[0].x*windowDimension.getWidth());
+			int caliby = (int)(calibrationPoints[0].y*windowDimension.getHeight());
+			double closestdist = Math.sqrt( Math.pow((x-calibx),2)+Math.pow((y-caliby),2) );
+			Point2D.Double closestPoint = calibrationPoints[0];
+			for(Point2D.Double calibPoint: calibrationPoints){
+				calibx = (int)(calibPoint.x*windowDimension.getWidth());
+				caliby = (int)(calibPoint.y*windowDimension.getHeight());
+				double dist = Math.sqrt( Math.pow((x-calibx),2)+Math.pow((y-caliby),2) );
+				if(dist < closestdist){
+					closestdist = dist;
+					closestPoint = calibPoint;
 				}
-				
 			}
+			calibx = (int)(closestPoint.x*windowDimension.getWidth());
+			caliby = (int)(closestPoint.y*windowDimension.getHeight());
+			grphx.drawLine(x, y, calibx, caliby);
 			i++;
 		}
-		System.out.println(calibrationData.length);
+		//System.out.println(calibrationData.length);
 	}
 	
 	public CalibrationStatusDisplay(JFrame frame, Point2D.Double[] calibrationPoints, Point2D.Double[] calibrationData){
 		super.setBackground(Color.darkGray);
 		super.setOpaque(true);
 		
+		this.windowDimension = new Dimension(600,300);
 		this.calibrationPoints = calibrationPoints;
 		this.calibrationData = calibrationData;
 		
