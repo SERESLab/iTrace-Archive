03cb30b163c5161bc6d8fe53b552053268065fae
Ben Clark <onebenclark@gmail.com>
2017-02-02 15:00:41 -0500

    Adds enginetest project. Will be removed or renamed once the kinks have been worked out.

--- src/edu/ysu/itrace/EngineSocket.java
+++ src/edu/ysu/itrace/EngineSocket.java
@@ -5,27 +5,52 @@ import java.net.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class EngineSocket {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+public class EngineSocket implements PaintListener {
 	Socket socket;
 	BufferedReader reader;
-	String data;
+	String data = "";
 	Timer timer;
-	
+	Shell shell;
 	
 	EngineSocket(){
 		timer = new Timer();
+		
 		try{
 			socket = new Socket("localhost", 8080);
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			System.out.println(socket.isConnected());
+			
 			timer.schedule(new TimerTask(){
 
 				@Override
 				public void run() {
+					
 					try {
 						if(reader.ready()){
 							data = reader.readLine();
-							System.out.println(data);
+							int end = 0;
+							//while(end != data.length() && (int)data.charAt(end) != 0) end++;
+							//if(end != data.length()) 
+							if(data.length() >= 9)
+								data = data.substring(data.length()-9, data.length());
+							//System.out.println(data);
+							PlatformUI.getWorkbench().getDisplay().asyncExec( new Runnable(){
+
+								@Override
+								public void run() {
+									shell.redraw();
+									
+								}
+								
+							});
 						}
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
@@ -35,8 +60,18 @@ public class EngineSocket {
 				}
 				
 			}, 0,10);
+			shell = new Shell(SWT.DOUBLE_BUFFERED);
+			shell.setSize(1000,100);
+			shell.addPaintListener(this);
+			shell.setVisible(true);
 		}catch(IOException ex){
 			ex.printStackTrace();
 		}
+	}
+
+	@Override
+	public void paintControl(PaintEvent pe) {
+		pe.gc.drawText(data, 0, 0);
+		
 	}
 }
